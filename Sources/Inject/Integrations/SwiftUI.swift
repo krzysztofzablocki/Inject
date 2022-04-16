753c63014e92f752f63fb39394761628c4f0d627
tbren <contact@apptekstudios.com>
2022-04-16 21:58:34 +1000

    Formatting

--- Sources/Inject/Integrations/SwiftUI.swift
+++ Sources/Inject/Integrations/SwiftUI.swift
@@ -5,11 +5,12 @@ import SwiftUI
 public extension SwiftUI.View {
     func enableInjection() -> some SwiftUI.View {
         _ = Inject.load
+        
         // Use AnyView in case the underlying view structure changes during injection.
         // This is only in effect in debug builds.
         return AnyView(self)
     }
-    
+
     func onInjection(callback: @escaping (Self) -> Void) -> some SwiftUI.View {
         onReceive(Inject.observer.objectWillChange, perform: {
             callback(self)
